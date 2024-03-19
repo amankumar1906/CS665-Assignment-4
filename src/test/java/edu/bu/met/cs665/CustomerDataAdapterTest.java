5f2dbe0b2d5068348ada840caf8b58037fffa9ec
Aman Kumar <amankumar7506@gmail.com>
2024-03-19 18:59:39 -0400

    remove redundant code

--- src/test/java/edu/bu/met/cs665/CustomerDataAdapterTest.java
+++ src/test/java/edu/bu/met/cs665/CustomerDataAdapterTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertTrue;
 
 import edu.bu.met.cs665.example1.CustomerDataAdapter;
 import edu.bu.met.cs665.example1.CustomerDataHttps;
-import edu.bu.met.cs665.example1.CustomerDataHttps;
 import edu.bu.met.cs665.example1.LegacyCustomerSystem;
 import org.junit.Before;
 import org.junit.Test;
